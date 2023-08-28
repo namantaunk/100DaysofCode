0aad7c29ae01626dc800193dc979b5c72b46dcad
Naman Taunk <112823340+namantaunk@users.noreply.github.com>
2023-08-28 23:49:26 +0530

    Update day1.java

--- day1.java
+++ day1.java
@@ -1,1 +1,18 @@
-
+// Given an array nums of integers, return how many of them contain an even number of digits.
+ class Solution {
+    public int findNumbers(int[] nums) {
+        
+        int evenNum=0;
+        for (int i=0; i<nums.length; i++)
+        {int count=0;
+            while(nums[i]>0){
+                nums[i]= nums[i]/10 ;
+                count++;
+            }
+            if(count%2==0){
+                evenNum++;
+            }
+        }
+        return(evenNum);
+    }
+}
