1452793a47e79677db27471402fb9b653309b86a
Naman Taunk <112823340+namantaunk@users.noreply.github.com>
2023-08-30 14:23:35 +0530

    Create day3.java

---
+++ day3.java
@@ -0,0 +1,16 @@
+// Given a fixed-length integer array arr, duplicate each occurrence of zero, shifting the remaining elements to the right.
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        for(int i=0; i<arr.length; i++)
+        {
+           // if(i==arr.length-1) break;
+           if(arr[i]==0){
+                   for(int j=arr.length-1; j>=i+1;j--)
+                {
+                    arr[j]=arr[j-1];
+                }
+                i++;           
+           }
+        }       
+            }
+        }
