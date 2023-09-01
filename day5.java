a572c856c3110242bb3a96e8ce63c95a612e2688
Naman Taunk <112823340+namantaunk@users.noreply.github.com>
2023-09-01 13:54:11 +0530

    Update day5.java

--- day5.java
+++ day5.java
@@ -15,3 +15,30 @@ public class Questions {
 		arr[index1]= temp ;
 	}
 }
+
+
+// 2. Maximum number in the array
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class Questions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int[] arr= {3,7,4,2,8,5,4};
+       int result = max(arr);
+       System.out.println(result);
+       
+	
+	}
+	static int max(int[] arr) {
+	    int maxVal=arr[0];
+		for (int i = 0; i < arr.length; i++) {
+			if(arr[i]>maxVal)
+				maxVal= arr[i];
+		}
+	    return maxVal; 
+	}
+	
+
+}
