55c52c56021898ef62d72a09c547cea4e8318d17
Naman Taunk <112823340+namantaunk@users.noreply.github.com>
2023-09-04 19:12:09 +0530

    Update day8.java

--- day8.java
+++ day8.java
@@ -1,4 +1,4 @@
-// Find Smallest Letter Greater Than Target.
+//Q.1 Find Smallest Letter Greater Than Target.
 /*You are given an array of characters letters that is sorted in non-decreasing order, and a character target. There are at least two different characters in letters.
 
 Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters. */
@@ -25,4 +25,30 @@ class Solution {
         }
     return letters[start % letters.length];
     }
+}
+
+//--------------------------------------------------------------------------------------------------------
+
+// Q.2 Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+       int start= 0;
+		int end = nums.length -1 ;
+		while(start<=end)
+		{
+			int mid= start + (end-start)/2;
+			 if(target>nums[mid])
+			 {
+				 start=mid+1;
+			 }
+			 else if(target<nums[mid])
+			 {
+				 end= mid-1;
+			 }
+			 else {
+				 return mid;
+			 }
+        } 
+        return start;
+    }     
 }
