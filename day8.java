eee08d2551d5c5d5dc2e3f619813289a0d2d9bad
Naman Taunk <112823340+namantaunk@users.noreply.github.com>
2023-09-04 19:44:59 +0530

    Update day8.java

--- day8.java
+++ day8.java
@@ -78,3 +78,21 @@ class Solution {
        return (sum1-sum2);
     }
 }
+
+
+//Q.4 Given a m x n matrix grid which is sorted in non-increasing order both row-wise and column-wise, return the number of negative numbers in grid.
+
+class Solution {
+    public int countNegatives(int[][] grid) {
+        int count=0;
+        for(int r=0; r<grid.length;r++)
+        {
+            for(int c=0; c<grid[r].length;c++)
+            {
+                if(grid[r][c]<0)
+                count++;
+            }
+        }
+    
+    return count;}
+}
