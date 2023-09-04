468363be34eb5ee0a62e017a9be510cf28dfd861
Naman Taunk <112823340+namantaunk@users.noreply.github.com>
2023-09-04 19:28:47 +0530

    Update day8.java

--- day8.java
+++ day8.java
@@ -52,3 +52,29 @@ class Solution {
         return start;
     }     
 }
+
+
+//-------------------------------------------------------------------------------------------------------
+
+//Q.3 Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+/* Input: nums = [3,0,1]
+   Output: 2
+   Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums. */
+
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n= nums.length;
+        int sum1=0;
+        int sum2=0;
+        for(int i=0; i<=n;i++)
+        {
+            sum1= sum1+i;
+        }
+        for(int i=0;i<n;i++)
+        {
+            sum2= sum2+ nums[i];
+        }
+       return (sum1-sum2);
+    }
+}
