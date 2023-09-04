89b4cfe4c8603a61b8dbe81977743a71714be2bc
Naman Taunk <112823340+namantaunk@users.noreply.github.com>
2023-09-04 14:50:42 +0530

    Update day8.java

--- day8.java
+++ day8.java
@@ -1,4 +1,9 @@
 // Find Smallest Letter Greater Than Target.
+/*You are given an array of characters letters that is sorted in non-decreasing order, and a character target. There are at least two different characters in letters.
+
+Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters. */
+
+
 
 class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
