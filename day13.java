b8a6f9494899d04e87fb53ac3613d90e25d25f99
Naman Taunk <112823340+namantaunk@users.noreply.github.com>
2023-09-12 01:08:44 +0530

    Create day13.java

---
+++ day13.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int start=0;
+        int end= nums.length-1;
+        int min= Integer.MAX_VALUE;
+        while(start<=end)
+        {
+            int mid= start + (end-start)/2;
+            if(nums[start]<=nums[mid]){
+                min= min<nums[start]?min:nums[start];
+                start=mid+1;
+            }
+            else{
+                min=min<nums[mid+1]?min:nums[mid];
+                end=mid-1;            
+            }
+        }
+  return min;  }
+}
