af1e2eec21a8bf4a4e172a796bbf91a86bf6c3f1
Naman Taunk <112823340+namantaunk@users.noreply.github.com>
2023-09-13 23:46:48 +0530

    Update day15.java

--- day15.java
+++ day15.java
@@ -10,7 +10,7 @@ public class Sort{
     for(int i=0;i<arr.length;i++){
       int last= arr.length-i-1;    
       int maxIndex= getMaxIndex(arr,0;last);
-      swap(,arr,maxIndex,last);
+      swap(arr,maxIndex,last);
     }
   }
 
