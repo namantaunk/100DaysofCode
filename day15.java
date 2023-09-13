eb33efda1d838923c7247c203c85557d9f565691
Naman Taunk <112823340+namantaunk@users.noreply.github.com>
2023-09-13 23:46:12 +0530

    Update day15.java

--- day15.java
+++ day15.java
@@ -1,1 +1,35 @@
+// Selection sort
+public class Sort{
+  public static void main(String[] args){
+   int[] arr= {3,4,2,5,1};
+   selection(arr);
+    System.out.println(Arrays.toString(arr));
+  }
 
+  static void selection(int[] arr){
+    for(int i=0;i<arr.length;i++){
+      int last= arr.length-i-1;    
+      int maxIndex= getMaxIndex(arr,0;last);
+      swap(,arr,maxIndex,last);
+    }
+  }
+
+  static int getMaxIndex(int[] arr,int start,int end){
+     int max= start;
+    for(int i=0;i<arr.length;i++)
+      {
+        if(arr[i]>arr[max])
+        {
+          max=i;
+        }
+      }
+   return max; 
+  }
+
+  static void swap(int[] arr,int first,int second)
+  {
+    int temp= first;
+    first=second;
+    second= temp;
+  }
+}
