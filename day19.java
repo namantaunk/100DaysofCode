f0f4b084aa94a01f67f5702ff83e5e739362984c
Naman Taunk <112823340+namantaunk@users.noreply.github.com>
2023-09-18 00:35:22 +0530

    Update day19.java

--- day19.java
+++ day19.java
@@ -1,3 +1,14 @@
+/*nput Format:
+The first line contains a string . The second line contains another string . The strings are comprised of only lowercase English letters.
+
+Output Format
+
+There are three lines of output:
+For the first line, sum the lengths of  and .
+For the second line, write Yes if  is lexicographically greater than  otherwise print No instead.
+For the third line, capitalize the first letter in both  and  and print them on a single line, separated by a space. */
+
+
 import java.io.*;
 import java.util.*;
 
