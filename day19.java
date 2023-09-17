b26b475e14583df3b782732064e6dc2ae425d41f
Naman Taunk <112823340+namantaunk@users.noreply.github.com>
2023-09-18 01:04:26 +0530

    Update day19.java

--- day19.java
+++ day19.java
@@ -37,3 +37,33 @@ public class Solution {
 
 
 
+
+
+// Q2. Substring Problem.
+
+
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        String sub= S.substring(start,end);
+        System.out.println(sub);
+    }
+}
+
+
+
+
+
+
+
